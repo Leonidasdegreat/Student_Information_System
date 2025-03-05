bd52c4c28a52fa3cb72c2b662032345c06967f03
Bryanskie <bryantagaan12@gmail.com>
2025-03-05 22:50:29 +0800

    With Db seed

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -3,6 +3,8 @@ namespace Database\Factories;
 
 use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class StudentFactory extends Factory
 {
@@ -13,8 +15,11 @@ class StudentFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password123'), // Default password
             'address' => $this->faker->address,
+            'role' => $this->faker->randomElement(['student', 'admin']),
             'age' => $this->faker->numberBetween(18, 25),
+            'email_verified_at' => now(), // Auto-verified emails
         ];
     }
 }
